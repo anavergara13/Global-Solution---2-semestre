15b0737a4c16a3909b5cc8324ed7e0052056873a
Gabriel Santi <gabrielspessoti@gmail.com>
2026-06-09 21:20:26 -0300

    Cria o servidor do projeto e organiza os arquivos do site

---
+++ app.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const express = require('express');
+const helmet = require('helmet');
+const session = require('express-session');
+
+const app = express();
+
+app.use(helmet({ contentSecurityPolicy: false })); // CSP off: front usa CDNs (Leaflet/Chart)
+app.use(express.json());
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'orbitasafe-dev-secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { httpOnly: true, sameSite: 'lax' }
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
