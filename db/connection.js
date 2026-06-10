247f6ac645b8c6c0c291648567a73ca4eb41d1b7
Gabriel Santi <gabrielspessoti@gmail.com>
2026-06-09 21:24:54 -0300

    Ativa a verificação das relações entre as tabelas

--- db/connection.js
+++ db/connection.js
@@ -4,6 +4,7 @@ const Database = require('better-sqlite3');
 const dbPath = process.env.DB_PATH || path.join(__dirname, 'orbitasafe.db');
 const db = new Database(dbPath);
 db.pragma('journal_mode = WAL');
+db.pragma('foreign_keys = ON');
 
 db.exec(`
   CREATE TABLE IF NOT EXISTS usuarios (
